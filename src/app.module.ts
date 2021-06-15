114fe021b365f7d16a6847e64201735bdb31f1f5
Collinsboss18 <abadaikecollins@gmail.com>
2021-06-15 21:55:41 +0100

    typeorm with graphqlmodule funnctional

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://channel18:password18@cluster0.xxiab.mongodb.net/orm'),
+    ItemsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
